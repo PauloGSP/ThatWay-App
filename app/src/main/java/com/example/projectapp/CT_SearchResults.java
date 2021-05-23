cc633499b25ce1693a759771285ee8820588e57b
Ricardo Rodriguez <ricardorodriguez@ua.pt>
2021-05-23 23:59:12 +0100

    activies create and basic elements added

---
+++ app/src/main/java/com/example/projectapp/CT_SearchResults.java
@@ -0,0 +1,14 @@
+package com.example.projectapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CT_SearchResults extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ct_search_results);
+    }
+}
