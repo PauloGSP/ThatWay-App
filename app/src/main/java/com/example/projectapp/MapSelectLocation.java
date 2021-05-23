3a73113a1d06af390bb8eae560d4d817bda19bab
Ricardo Rodriguez <ricardorodriguez@ua.pt>
2021-05-23 23:23:27 +0100

    aesthetic alts. and title removal

---
+++ app/src/main/java/com/example/projectapp/MapSelectLocation.java
@@ -0,0 +1,14 @@
+package com.example.projectapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MapSelectLocation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_select_location2);
+    }
+}
