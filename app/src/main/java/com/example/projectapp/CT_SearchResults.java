f3788025be74d21e4ffb415cd19bda46008f5e30
Ricardo Rodriguez <ricardorodriguez@ua.pt>
2021-05-24 18:23:09 +0100

    Trip class parameters and CT_SearchResults trip creations (array included)

--- app/src/main/java/com/example/projectapp/CT_SearchResults.java
+++ app/src/main/java/com/example/projectapp/CT_SearchResults.java
@@ -1,12 +1,55 @@
 package com.example.projectapp;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
 
+import java.io.File;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Scanner;
+
+@RequiresApi(api = Build.VERSION_CODES.O)
 public class CT_SearchResults extends AppCompatActivity {
+
+
+    //temos de receber os filtros selecionados a partir do CT_LocationFilters
+
+    // arrays/ficheiros necessários
+    ArrayList<Trip> list_of_trips = new ArrayList<Trip>();  //contem todas as viagens
+
+
+    //ler um ficheiro e adicionar objetos Trip para list_of_trips
+    Scanner sc = new Scanner(new File("trips.txt"));
+    while (sc.hasNextLine()) {
+        String line = sc.nextLine();
+        String[] trip_info = line.split("/t");
+
+        String origin = trip_info[0];
+        String destiny = trip_info[1];
+        LocalTime departure_time = LocalTime.parse(trip_info[2]);
+        LocalTime arrival_time = LocalTime.parse(trip_info[3]);
+        String transport_type = trip_info[4];
+        Double price = Double.parseDouble(trip_info[5]);
+        long travelling_time = ChronoUnit.MINUTES.between(l1,l2);
+        String departure_date_string = trip_info[6];
+        DateFormat df = new SimpleDateFormat("dd/mm/yyyy");
+        Date departure_date = df.parse(departure_date_string);
+        String place_of_departure = trip_info[7];
+        String place_of_arrival = trip_info[8];
+
+        //ponto de origem / ponto de destino / hora saída / hora chegada / tipo transporte / preço / data de saída (se for DAILY repete-se todos os dias) / coordenadas do ponto de saida / coordenadas do ponto de chegada
+        Trip trip = new Trip(origin,destiny,departure_time,arrival_time,transport_type,price,travelling_time,departure_date,place_of_departure,place_of_arrival);
+        list_of_trips.add(trip);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/example/projectapp/Trip.java
+++ app/src/main/java/com/example/projectapp/Trip.java
@@ -1,23 +1,34 @@
 package com.example.projectapp;
 
+import java.time.LocalTime;
 import java.util.Date;
 
 public class Trip {
 
     private String origin;
     private String destiny;
-    private Date departure_time;
-    private Date arrival_time;
-    private String transport_type;      //maybe criar um ENUM para também associar um ícone a um tipo de transporte
+    private LocalTime departure_time;
+    private LocalTime arrival_time;
+    private String transport_type;
     private Double price;
-    private Date travelling_time = arrival_time - departure_time;   //fazer isto no construtor maybe
+    private long travelling_time;
     private Date departure_date;
-    private String place_of_departure;  //coordenadas? (tratamos disto dps)
+    private String place_of_departure;
     private String place_of_arrival;
 
     //construtores (devemos ter vários consoante os parâmetros introduzidos)
-    public Trip() {
-
+    public Trip(String origin, String destiny, LocalTime departure_time, LocalTime arrival_time, String transport_type, Double price,
+                long travelling_time, Date departure_date, String place_of_departure, String place_of_arrival) {
+        this.origin = origin;
+        this.destiny = destiny;
+        this.departure_time = departure_time;
+        this.arrival_time = arrival_time;
+        this.transport_type = transport_type;
+        this.price = price;
+        this.travelling_time = travelling_time;
+        this.departure_date = departure_date;
+        this.place_of_departure = place_of_departure;
+        this.place_of_arrival = place_of_arrival;
     }
 
     public String getOrigin() {
