96882ba6a2827b05cc6afa655d54514076a88516
Ricardo Rodriguez <ricardorodriguez@ua.pt>
2021-06-13 00:13:18 +0100

    ^.^

--- app/src/main/java/com/example/projectapp/CT_SearchResults.java
+++ app/src/main/java/com/example/projectapp/CT_SearchResults.java
@@ -2,10 +2,16 @@ package com.example.projectapp;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -22,13 +28,37 @@ public class CT_SearchResults extends AppCompatActivity {
     public static boolean metro = true;
     public static String order;
 
-    //mostrar os resultados
-    public void loadResults(String origin, String destiny) {
+    private int currentPage = 1;
+    private int maxPages;
 
+    //mostrar os resultados
+    public void loadResults(int current) {
+
+        Button nextBtn = (Button) findViewById(R.id.btnNext);
         TextView lblOrigin = (TextView) findViewById(R.id.lblOrigin);
         TextView lblDestiny = (TextView) findViewById(R.id.lblDestiny);
+
+        if (current >= maxPages) {
+            //passar para a proxima atividade
+
+            Intent goToRoute = new Intent(getApplicationContext(), RouteAllDetails.class);
+            startActivity(goToRoute);
+
+        } else if (current == maxPages -1) {
+            nextBtn.setText("SAVE ROUTE");
+
+        } else {
+            nextBtn.setText("NEXT (" + current + "/" + maxPages + ")");
+        }
+
+        String origin = selected_trips.get(current - 1);
+        String destiny = selected_trips.get(current);
+
         lblOrigin.setText(origin);
         lblDestiny.setText(destiny);
+
+        //mostrar resultados no recyclerview
+
 
 
     }
@@ -42,13 +72,30 @@ public class CT_SearchResults extends AppCompatActivity {
         getSupportActionBar().hide();
         setContentView(R.layout.activity_ct_search_results);
 
+        maxPages = selected_trips.size() - 1;
+
+        TripAdapter adapter = new TripAdapter(this, MainActivity.allTrips);
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+        recyclerView.setAdapter(adapter);
+
+
+
+        //load da primeira pagina
+        loadResults(currentPage);
+
+        Button nextBtn = (Button) findViewById(R.id.btnNext);
+        nextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //sempre que se clica NEXT mostra a proxima página de resultados se tudo tiver selecionado ou, se for a ultima pagina,
+                //passa para a proxima activity
+                loadResults(++currentPage);
+
+            }
+        });
+
         System.out.println("SEARCH RESULTS - TRIP INFO");
         for (String s : selected_trips) System.out.println(s);
 
-        for (int t = 0; t < selected_trips.size()-1 ; t++) {
-            String origin = selected_trips.get(t);
-            String destiny = selected_trips.get(t+1);
-            loadResults(origin,destiny);
-        }
     }
 }
--- app/src/main/java/com/example/projectapp/TripAdapter.java
+++ app/src/main/java/com/example/projectapp/TripAdapter.java
@@ -16,33 +16,36 @@ import org.w3c.dom.Text;
 import java.util.ArrayList;
 
 public class TripAdapter extends ArrayAdapter<Trip> {
-    public TripAdapter(Context context, ArrayList<Trip> trips){
 
-        super(context,0, trips);
+    public TripAdapter(Context context, ArrayList<Trip> trips){
+        super(context, R.layout.row_trip, trips);
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+
         // Get the data item for this position
         Trip trip = getItem(position);
+
         // Check if an existing view is being reused, otherwise inflate the view
         if (convertView == null) {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.row_trip, parent, false);
         }
+
         // Lookup view for data population
-        ConstraintLayout MainContainer = (ConstraintLayout) convertView.findViewById(R.id.constraintLayout3) ;
+        ConstraintLayout MainContainer = (ConstraintLayout) convertView.findViewById(R.id.trip_container) ;
         ImageView Image=(ImageView) convertView.findViewById(R.id.transportTypeIcon);
         TextView TransportType = (TextView) convertView.findViewById(R.id.transportTypeText);
         TextView scheduleTimeText = (TextView) convertView.findViewById(R.id.scheduleTimeText);
         TextView tickerPriceText = (TextView) convertView.findViewById(R.id.tickerPriceText);
         Button addtrip = (Button) convertView.findViewById(R.id.addTripBtn);
         Button moreinfo = (Button) convertView.findViewById(R.id.moreInfoBtn);
-        ImageButton Timeicon =(ImageButton) convertView.findViewById(R.id.iconTime);
-        ImageButton moneyicon =(ImageButton) convertView.findViewById(R.id.iconMoney);
+        ImageButton Timeicon = (ImageButton) convertView.findViewById(R.id.iconTime);
+        ImageButton moneyicon = (ImageButton) convertView.findViewById(R.id.iconMoney);
 
         // Populate the data into the template view using the data object
         TransportType.setText(trip.getTransport_type());
-        tickerPriceText.setText( trip.getPrice().toString());
+        tickerPriceText.setText(trip.getPrice().toString());
         addtrip.setClickable(true);
         moreinfo.setClickable(true);
 
