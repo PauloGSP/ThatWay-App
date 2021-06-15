65e0d218bcef0d167f073a050546baf5df688c86
Ricardo Rodriguez <ricardorodriguez@ua.pt>
2021-06-15 01:11:24 +0100

    route details total travelling time + total price and name validation

--- app/src/main/java/com/example/projectapp/Route.java
+++ app/src/main/java/com/example/projectapp/Route.java
@@ -7,7 +7,7 @@ public class Route {
     public static ArrayList<Route> savedRoutes = new ArrayList<Route>();
     public static Route currentRoute;
     private String title;
-    private String total_travelling_time;
+    private long total_travelling_time;
     private Double total_price;
     private ArrayList<Trip> list_of_trips;
 
@@ -15,6 +15,8 @@ public class Route {
     public Route(String title,ArrayList<Trip> routeTrips) {
         this.title = title;
         this.list_of_trips = routeTrips;
+        getTotal_travelling_time();
+        getTotal_price();
     }
 
     //metodos para calcular o total_travelling_time e o total_price
@@ -27,15 +29,29 @@ public class Route {
         this.title = title;
     }
 
-    public String getTotal_travelling_time() {
-        return total_travelling_time;
+    public long getTotal_travelling_time() {
+        long travellingTime = 0;
+        for (Trip t : list_of_trips) {
+            if (t != null) {
+                travellingTime += t.getTravelling_time();
+            }
+        }
+        this.total_travelling_time = travellingTime;
+        return travellingTime;
     }
 
-    public void setTotal_travelling_time(String total_travelling_time) {
+    public void setTotal_travelling_time(long total_travelling_time) {
         this.total_travelling_time = total_travelling_time;
     }
 
     public Double getTotal_price() {
+        double total_price = 0;
+        for (Trip t : list_of_trips) {
+            if (t != null) {
+                total_price += t.getPrice();
+            }
+        }
+        this.total_price = total_price;
         return total_price;
     }
 
--- app/src/main/java/com/example/projectapp/RouteAllDetails.java
+++ app/src/main/java/com/example/projectapp/RouteAllDetails.java
@@ -3,8 +3,13 @@ package com.example.projectapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class RouteAllDetails extends AppCompatActivity {
 
@@ -15,5 +20,46 @@ public class RouteAllDetails extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_route_all_details);
+
+        Route route = Route.currentRoute;
+
+        EditText routeName = findViewById(R.id.routeName);
+        routeName.setText(route.getTitle());
+        routeName.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void afterTextChanged(Editable s) {}
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String currentTitle = routeName.getText().toString();
+                boolean validName = true;
+                for (Route r : Route.savedRoutes) {
+                    if (r.getTitle() == s) validName = false;
+                }
+                if (validName) {
+                    if (s.equals("")) {
+                        Toast.makeText(getApplicationContext(),"Please insert a route name", Toast.LENGTH_SHORT).show();
+                        routeName.setText(currentTitle);
+                    } else {
+                        Toast.makeText(getApplicationContext(),"Changed route name successfully", Toast.LENGTH_SHORT).show();
+                        route.setTitle(s.toString());
+                    }
+                } else {
+                    Toast.makeText(getApplicationContext(),"There's already a route with that title", Toast.LENGTH_SHORT).show();
+                    routeName.setText(currentTitle);
+                }
+            }
+        });
+
+
+        TextView totalTravellingTime = findViewById(R.id.totalTravellingTime);
+        totalTravellingTime.setText(route.getTotal_travelling_time() + " minutes");
+        TextView totalPrice = findViewById(R.id.totalPrice);
+        totalPrice.setText(route.getTotal_price().toString() + "€");
+
     }
 }
